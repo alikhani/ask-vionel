cd9ae6e7db8dcbc81f0c6e7cb89db511e394ce65
Nima <nima@trigd.com>
2015-04-10 12:46:42 +0200

    angular init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name askVionelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the askVionelApp
+ */
+angular.module('askVionelApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
