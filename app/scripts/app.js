ce922a4dce03b15073f8471fda270d628bef288b
Nima <nima@trigd.com>
2015-04-10 15:30:40 +0200

    done some stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,11 +19,205 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
+
+  .factory('Cookie', ['$location', function($location) {
+  return {
+    get: function(name){
+      var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+      if (result) {
+        return JSON.parse(result[1]);
+      }
+    },
+    set: function(name, value){
+      var d = new Date();
+      d.setMonth(d.getMonth() + 1);
+
+        var cookie = name + "=" + JSON.stringify(value) + '; path=/; domain=.'+$location.host()+'; expires=' + d.toUTCString() + ';';
+
+      document.cookie = cookie;
+    },
+    delete: function(name){
+      document.cookie = name+ '=; path=/; domain=.'+$location.host()+'; expires=Thu, 01-Jan-1970 00:00:01 GMT;';
+    }
+  };
+}])
+
+  .factory('principal', ['$q', '$http', '$timeout', 'Cookie', 
+  function($q, $http, $timeout, Cookie) {
+    var _identity;
+    var _authenticated = false;
+
+    return {
+      isIdentityResolved: function() {
+        return angular.isDefined(_identity);
+      },
+      isAuthenticated: function() {
+        return _authenticated;
+      },
+      getSessiontoken: function() {
+        _sessionToken = Cookie.get("user");
+        return _sessionToken;
+      },
+      isInRole: function(role) {
+        if (!_authenticated || !_identity.roles){ return false; }
+
+        return _identity.roles.indexOf(role) != -1;
+      },
+      isInAnyRole: function(roles) {
+        if (!_authenticated || !_identity.roles) { return false; }
+
+        for (var i = 0; i < roles.length; i++) {
+          if (this.isInRole(roles[i])) { return true; }
+        }
+
+        return false;
+      },
+      isCookiesAccepted: function() {
+        return Cookie.get("cookieacceptance");
+      },
+      acceptCookies: function() {
+        Cookie.set("cookieacceptance", true);
+      },
+      getUser: function() {
+        return _identity;
+      },
+      authenticate: function(identity) {
+        _identity = identity;
+        _authenticated = identity != null;
+
+        if (identity) {
+          // console.log("set user: ",identity);
+          Cookie.set("user",angular.toJson(identity));
+          // localStorage.setItem("user.identity", angular.toJson(identity)); 
+        } else {
+          // console.log("delete");
+          Cookie.delete("user");
+          localStorage.removeItem("user.portrait");
+          // Cookie.delete("sessionToken");
+          // localStorage.removeItem("user.identity");
+        }
+      },
+      userImage: function(type, image) {
+        if (type === 'SET') {
+          localStorage.setItem("user.portrait", angular.toJson(image));
+        } else {
+          if (localStorage.getItem("user.portrait") !== 'undefined') {
+            return angular.fromJson(localStorage.getItem("user.portrait"));
+          }
+          
+        }
+      },
+      authenticateTempUser: function(callback) {
+        // console.log("temp auth");
+
+
+        $http({method: 'post', url: 'http://127.0.0.1:3044/api/temp_auth'}).
+        success(function(data, status, headers, config) {
+          // console.log("success! status: ",status,", data: ",data);
+                var user = {
+                    "userToken": data.response.userToken,
+                    "sessionToken": data.response.sessionToken,
+                    "isTemp": true
+                };
+          // Cookie.set("user", angular.toJson(user));
+          // console.log("u: ",user);
+          callback(user);
+        });
+      },
+      identity: function(force) {
+        var deferred = $q.defer();
+
+        if (force === true) {
+          _identity = undefined;
+        }
+
+        // check and see if we have retrieved the identity data from the server. if we have, reuse it by immediately resolving
+        if (angular.isDefined(_identity)) {
+          deferred.resolve(_identity);
+
+          return deferred.promise;
+        }
+
+        var self = this;
+
+_identity = angular.fromJson(Cookie.get("user")/*localStorage.getItem("user.identity")*/);
+self.authenticate(_identity);
+deferred.resolve(_identity);
+
+return deferred.promise;
+}
+};
+}
+])
+
+  .factory('authorization', ['$rootScope', 'principal',
+  function($rootScope, principal) {
+    return {
+      authorize: function() {
+        return principal.identity()
+        .then(function() {
+          // console.log("auth-state: ",$state);
+          var isAuthenticated = principal.isAuthenticated();
+          var user = principal.getUser();
+
+          // console.log("auth: ",isAuthenticated,", user: ",user);
+
+          if(isAuthenticated || user) {
+            // console.log("there is a user");
+          } else {
+            // console.log("not auth");
+            principal.authenticateTempUser( function(resp) {
+              console.log("callback: ",resp);
+              principal.authenticate(resp);
+              // $state.transitionTo($state.current, $stateParams, {
+              //   reload: true,
+              //   inherit: false,
+              //   notify: true
+              // });
+            });
+          }
+          });
+      }
+    };
+  }
+  ])
+
+  .factory('sessionInjector', ['$q','$injector', '$rootScope', 'Cookie', function($q, $injector) {
+    var user;
+    var sessionInjector = {
+        request: function(config) {
+          var canceler = $q.defer();
+          var p = $injector.get('principal');
+          user = p.getUser();
+          // console.log("getUser: ",user,", id: ",p.isIdentityResolved(),", root: ",$rootScope.user,", cookie: ",Cookie.get("user"));
+          if (user) {
+            config.headers['X-Session-Token'] = user.sessionToken;
+          } else {
+
+            if(config.url !== "/api/temp_auth") {
+              config.timeout = canceler.promise;
+              canceler.resolve();
+            }
+
+          }
+          return config || $q.when(config);
+        }
+    };
+    return sessionInjector;
+  }])
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          authorize: ['authorization',
+          function(authorization) {
+            return authorization.authorize();
+          }
+          ]
+        }
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,30 @@
  * Controller of the askVionelApp
  */
 angular.module('askVionelApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    console.log("bla");
+
+    function querySender(query) {
+        var q = 'tags=genre:' + query;
+        var url = '/api/search?'+q+'&size=3&page=1';
+        $http({method: 'GET', url: url}).
+            success(function(data, status, headers, config) {
+                console.log("success! status: ",status,", data: ",data);
+                // callback(data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log("error! status: ",status,", data: ",data);
+                // callback(data);
+            });
+    }
+
+    $scope.question = {'query': 'action', 'question': 'this is question 1'};
 
     $scope.result = [
     {'title': "bla", 'thumbnailUrl': "/images/no-image3.jpg"},
@@ -21,10 +39,23 @@ angular.module('askVionelApp')
     {'title': "bla2", 'thumbnailUrl': "/images/no-image3.jpg"}
     ];
 
-    $scope.yes = function() {
+    $scope.yes = function(query) {
         console.log("yes");
+        querySender(query);
+        $scope.question = {'query': 'drama', 'question': 'this is question 2'};
+        $scope.result = [
+            {'title': "bla3", 'thumbnailUrl': "/images/no-image3.jpg"},
+            {'title': "bla4", 'thumbnailUrl': "/images/no-image3.jpg"},
+            {'title': "bla5", 'thumbnailUrl': "/images/no-image3.jpg"}
+        ];
     };
-    $scope.no = function() {
+    $scope.no = function(query) {
         console.log("no");
+        $scope.question = {'query': 'drama', 'question': 'this is question 3'};
+        $scope.result = [
+            {'title': "bla3", 'thumbnailUrl': "/images/no-image3.jpg"},
+            {'title': "bla4", 'thumbnailUrl': "/images/no-image3.jpg"},
+            {'title': "bla5", 'thumbnailUrl': "/images/no-image3.jpg"}
+        ];
     };
   });
